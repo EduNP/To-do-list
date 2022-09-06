35996798d7b264a5900eb53b71ca99c7b9333a1e
Eduardo Neves Paschoal <54189084+EduNP@users.noreply.github.com>
2022-09-06 20:17:18 -0300

    Creating Backend

---
+++ Backend/src/main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import request
+from flask_cors import CORS
+import json
+
+app = Flask(__name__)  
+CORS(app)
+
+@app.route("/")
+def index():
+    result = {}
+    result['itens'] = ["Bala","chocolate"]
+    return json.dumps(result)
+
+app.run(debug=True, host='0.0.0.0', port=5000)
